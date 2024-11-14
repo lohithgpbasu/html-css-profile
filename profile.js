200b87e4c0e7154c4c5a873db780bf5a7e503b8a
lohithgpbasu <lohithpraju81@gmal.com>
2024-11-14 22:35:30 +0530

    practiced with js

--- profile.js
+++ profile.js
@@ -1,13 +1,57 @@
 //declaring a varaible using let keyword
-let a = "Ball";
+/*let a = "Ball";
 a = "Bat";
-alert(a);
+console.log(a);
 
 //declaring a varaible using const keyword
 const b = "Apple";
-b = "Banana";
-alert(b);
+console.log(b)
 
+//declaring a variable using var keyword
 var c = "Cat";
 c = "Dog";
-alert(c);
+console.log(c);*/
+
+//trying out differenet data type
+
+/*let a = {
+    name: "Rahul",
+    age: 32
+};
+console.log(a);
+console.log(typeof(a.age));*/
+
+//control flow
+
+//if statements
+/*let name = "Jai";
+console.log(`Welcome..! to my profile ${name}.`);*/
+
+//if else statement
+/*let age = 10;
+if(age >= 18)
+{
+    console.log("You are adult");
+}
+else{
+    console.log("You are not adult");
+}*/
+
+//loops
+
+//for loop
+/*let hobbies = ["Gaming", "Reading", "Coding", "Exploring"]
+i = 1;
+for(let hobby of hobbies){
+    console.log(`Hobby ${i}: ${hobby}`);
+    i++;
+}*/
+
+
+//while loop
+/*let i = 0;
+while(i <= 5){
+    console.log("Hello " + i);
+    i++;
+}*/
+
