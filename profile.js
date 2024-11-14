a771699b2c8184ca163a55cbd2d76189c8df63bd
lohithgpbasu <lohithpraju81@gmal.com>
2024-11-14 19:25:37 +0530

    "Cleared a small bug in profile.html"

---
+++ profile.js
@@ -0,0 +1,13 @@
+//declaring a varaible using let keyword
+let a = "Ball";
+a = "Bat";
+console.log(a);
+
+//declaring a varaible using const keyword
+const b = "Apple";
+b = "Banana";
+console.log(b)
+
+var c = "Cat";
+c = "Dog";
+console.log(c);
